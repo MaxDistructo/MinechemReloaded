85ee0e7ded9d5b97d5f98b31b8dca9b225510b4e
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-04-03 09:38:12 -0500

    Finish Adding Nugget Elements

--- chemicals/Chemicals.java
+++ chemicals/Chemicals.java
@@ -14,7 +14,7 @@ public class Chemicals {
         while(i = 1, i++, i <= 118){
             if(chemicalsString[i] != null){
                 if(chemicalsString[i].equals("c")){
-                    chemicals = register(new ItemOre("carbon","coal"));
+                    chemicals = register(new ItemOre("carbon","nuggetCoal"));
                 }
                 else if(chemicalsString[i].equals("al")){
                     chemicals = register(new ItemOre("aluminum","nuggetAluminum"));
@@ -29,11 +29,48 @@ public class Chemicals {
                     chemicals = register(new ItemOre("cobalt","nuggetCobalt"));
                 }
                 else if(chemicalsString[i].equals("mg")){
-                    chemicals = register(new Itemore("magnesium","nuggetMagnesium"));
+                    chemicals = register(new ItemOre("magnesium","nuggetMagnesium"));
                 }
-                
-            chemicals = register(new ItemOre("chemical" + chemicalsString[i], "chemical" + chemicalsString[i]));
-        }
+                else if(chemicalsString[i].equals("ni")){
+                    chemicals = register(new ItemOre("nickel","nuggetNickel"));
+                }
+                else if(chemicasString[i].equals("cu")){
+                    chemicals = register(new ItemOre("copper", "nuggetCopper"));
+                }
+                else if(chemicasString[i].equals("ti")){
+                    chemicals = register(new ItemOre("titanium", "nuggetTitanium"));
+                }
+                else if(chemicalsString[i].equals("ag"){
+                    chemicals = register(new ItemOre("silver", "nuggetSilver"));
+                }
+                else if(chemicalsString[i].equals("sn"){
+                    chemicals = register(new ItemOre("tin", "nuggetTin"));
+                }
+                else if(chemicalsString[i].equals("w")){
+                    chemicals = register(new ItemOre("tungsten", "nuggetTungsten");
+                }
+                else if(chemicalsString[i].equals("os")){
+                    chemicals = register(new ItemOre("osmium", "nuggetOsmium"));
+                }
+                else if(chemicalsString[i].equals("ir")){
+                    chemicals = register(new ItemOre("iridium", "nuggetIridium");
+                }
+                else if(chemicalsString[i].equals("pt")){
+                    chemicals = register(new ItemOre("platinum", "nuggetPlatinum"));
+                }
+                else if(chemicalsString[i].equals("au")){
+                    chemicals = register(new ItemOre("gold", "nuggetGold"));
+                }
+                else if(chemicalsString[i].equals("pb")){
+                    chemicals = register(new ItemOre("lead", "nuggetLead"));
+                }
+                else if(chemicalsString[i].equals("u")){
+                    chemicals = register(new ItemOre("uranium", "nuggetUranium"));
+                }
+                else{
+                    chemicals = register(new ItemOre("chemical" + chemicalsString[i], "chemical" + chemicalsString[i]));
+                }
+            }
         else{
                 System.out.println("Ended Adding Chemicals");
             }
