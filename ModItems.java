b476dee00a99443a604f5ca1630f96fd20016612
MaxDistructo <maxdistructo@users.noreply.github.com>
2017-04-03 20:18:17 -0500

    Add From IDEA

--- ModItems.java
+++ ModItems.java
@@ -4,10 +4,11 @@ import maxdistructo.core.create.ItemBase;
 import maxdistructo.core.oredict.ItemOreDict;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import minechemreborn.chemicals.Chemicals;
 
 public class ModItems {
     public static void init(){
-
+        Chemicals.init();
     }
     private static <T extends Item> T register(T item) {
         GameRegistry.register(item);
---
+++ config/ConfigReader.java
@@ -0,0 +1,7 @@
+package minechemreborn.config;
+
+/**
+ * Created by mican on 4/3/2017.
+ */
+public class ConfigReader {
+}
