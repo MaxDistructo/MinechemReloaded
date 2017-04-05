d3ae626bb70ae527729f15ffa2649fe27bbb221f
MaxDistructo <micanjvollan@gmail.com>
2017-04-05 18:52:48 +0000

    Update CompoundChemicals.java Made Work with New Core Library

--- src/minechemreborn/chemicals/CompoundChemicals.java
+++ src/minechemreborn/chemicals/CompoundChemicals.java
@@ -2,11 +2,24 @@ package minechemreborn.chemicals;
 
 import minechemreborn.chemicals.Chemicals;
 import net.shadowfacts.shadowmc.item.ItemOreDict;
+import net.shadowfacts.shadowmc.item.ModItems;
 
-public class CompoundChemicals{
+public class CompoundChemicals extends ModItems{
     public static String compounds[] = {"h2o","c10h15n","c17h21no4","no4"};
-    public static ItemOreDict compound;
-public static void init(){
+    static ItemOreDict compound;
+    private static int i;
+public void init(){
+    i = 1;
+    do{
+        if(Chemicals.chemicalsString[i] != null){
+            compound = register((ItemOreDict)new ItemOreDict (Chemicals.chemicalsString[i] + "2"));
+        }
+        else{
+            i = 100;
+        }
+        
+    }
+    while(i < 100);
     
 }
 }
